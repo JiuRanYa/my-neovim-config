2bf9a1b454f458c7183dccaae1baf2a649146c4b
1084350607 <1084350607@qq.com>
2023-03-31 17:46:08 +0800

    feat: add go to definition

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -62,6 +62,7 @@ keymap("n", "rn", "<cmd>Lspsaga rename<CR>", { silent = true })
 -- also support open/vsplit/etc operation check definition_action_keys
 -- support tagstack C-t jump back
 keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+keymap("n", "gf", "<cmd>Lspsaga goto_definition<CR>", { silent = true })
 
 -- Show line diagnostics
 keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
